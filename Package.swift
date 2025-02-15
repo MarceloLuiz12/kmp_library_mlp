dc52fb22bc308eee898403e80363287b33f10c22
Marcelo Luiz Pinheiro <106930566+MarceloLuiz12@users.noreply.github.com>
2025-02-15 14:37:42 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,17 +2,16 @@
 import PackageDescription
 
 let package = Package(
-   name: "Library",
+   name: "library",
    platforms: [
      .iOS(.v14),
    ],
    products: [
-      .library(name: "Library", targets: ["Library"])
+      .library(name: "library", targets: ["library"])
    ],
    targets: [
       .binaryTarget(
-         name: "Library",
-         url: "https://github.com/MarceloLuiz12/kmp_library_mlp/releases/download/1.0.0/XCFrameworks.zip",
-         checksum:"13edd9ebf3fcff01a977644de4f80c6b1c6d514311b36d56fadb58fd45951a78")
+         name: "library",
+         url: "https://github.com/MarceloLuiz12/kmp_library_mlp/releases/download/1.0.0/XCFrameworks.zip")
    ]
 )
