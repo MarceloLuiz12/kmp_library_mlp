4878f945325e8e563f2f68ee9c3865403dff2438
Marcelo Luiz Pinheiro <106930566+MarceloLuiz12@users.noreply.github.com>
2025-02-17 16:08:32 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "library",
-         url: "https://github.com/MarceloLuiz12/kmp_library_mlp/releases/download/1.0.3/XCFrameworks.zip",
+         url: "https://github.com/MarceloLuiz12/kmp_library_mlp/releases/download/1.0.4/XCFrameworks.zip",
          checksum: "ebf2b43913ef1032b50afa2586b7057397aad6a8bde48a162b265c94cdd0faed"
       )
    ]
